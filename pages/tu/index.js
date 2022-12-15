99d1e77524b44950f72952749f9d6fd0c369b6ba
Nhat Tien <ledangnhattien04@gmail.com>
2022-12-15 15:39:40 +0700

    fix trang tu

---
+++ pages/tu/index.js
@@ -0,0 +1,13 @@
+import Layout from "../../components/Layout";
+
+
+export default function Tu() {
+    return (
+        <Layout>
+            <main>
+                Đây là trang về phần lý thuyết Từ
+                sẽ có một danh sách các bài viết ở đây
+            </main>
+        </Layout>
+    )
+}
