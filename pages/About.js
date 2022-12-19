b3fb34297829796ffe2967cc7759ecfb43d3bc9f
Nhat Tien <ledangnhattien04@gmail.com>
2022-12-19 22:25:16 +0700

    fix quote

--- pages/About.js
+++ pages/About.js
@@ -9,9 +9,9 @@ export default function About() {
         <Layout>
         <main className="px-10 w-full">
         <Image src='/companylogo.png' className="mx-auto" width={400} height={200} alt='logo cong ty'></Image>
-        <p className="text-left font-noto-serif text-4xl font-semibold italic">"Thành trì vĩ đại</p> 
+        <p className="text-left font-noto-serif text-4xl font-semibold italic">&quot;Thành trì vĩ đại</p> 
         <p className="text-left font-noto-serif text-2xl italic">được xây nên từ</p> 
-        <p className="text-left font-noto-serif text-4xl font-semibold italic">một viên gạch nhỏ đầu tiên..."</p>
+        <p className="text-left font-noto-serif text-4xl font-semibold italic">một viên gạch nhỏ đầu tiên...&quot;</p>
         </main>
         </Layout>
     )
